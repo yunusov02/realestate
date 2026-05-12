753a0a3eb6964cc9a5a05db169cd9194c82e799b
Asilbek <englishisakovich@gmail.com>
2026-05-12 15:45:24 +0500

    Auth started

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+from core.softdelete import SoftDelete
+
+class User(AbstractBaseUser, SoftDelete):
+    
+    pass
+
+
